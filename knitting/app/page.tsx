8d3716fa92b85955e99e9f3b89431415bc691d3f
zoep30 <zoe.pintelon@student.kuleuven.be>
2025-11-04 16:55:45 +0100

    Homepage styling

--- knitting/app/page.tsx
+++ knitting/app/page.tsx
@@ -11,50 +11,52 @@ export default async function Home() {
   const users = data.users; //extract array
 
   return (
-    <div>
-      {/* <p className="text-[10rem] underline text-center">Hello World!</p> */}
-      <Image
-        src="/HomePageImages/image1.png"
-        alt="First decorative image"
-        width={187}
-        height={250}
-        className=""
-      />
-      <Image
-        src="/HomePageImages/image2.png"
-        alt="Second decorative image"
-        width={290}
-        height={315}
-        className=""
-      />
-      <Image
-        src="/HomePageImages/image3.png"
-        alt="Third decorative image"
-        width={256}
-        height={308}
-        className=""
-      />
-      <Image
-        src="/HomePageImages/image4.png"
-        alt="Fourth decorative image"
-        width={216}
-        height={215}
-        className=""
-      />
-      <Image
-        src="/HomePageImages/image5.png"
-        alt="Fifth decorative image"
-        width={206}
-        height={224}
-        className=""
-      />
-      <Image
-        src="/HomePageImages/image6.png"
-        alt="Sixth decorative image"
-        width={273}
-        height={301}
-        className=""
-      />
+    <div className='flex flex-col items-center text-center px-4 py-8 space-y-10'>
+      
+      <div className="flex flex-wrap justify-center gap-6">
+        <Image
+          src="/HomePageImages/image1.png"
+          alt="First decorative image"
+          width={187}
+          height={250}
+          className=""
+        />
+        <Image
+          src="/HomePageImages/image2.png"
+          alt="Second decorative image"
+          width={290}
+          height={315}
+          className=""
+        />
+        <Image
+          src="/HomePageImages/image3.png"
+          alt="Third decorative image"
+          width={256}
+          height={308}
+          className=""
+        />
+        <Image
+          src="/HomePageImages/image4.png"
+          alt="Fourth decorative image"
+          width={216}
+          height={215}
+          className=""
+        />
+        <Image
+          src="/HomePageImages/image5.png"
+          alt="Fifth decorative image"
+          width={206}
+          height={224}
+          className=""
+        />
+        <Image
+          src="/HomePageImages/image6.png"
+          alt="Sixth decorative image"
+          width={273}
+          height={301}
+          className=""
+        />
+      </div>
 
 
       <p className="text-[2rem] mt-10">Want to stop fast fashion and be more sustainable by making your own beautiful clothes? Join our step-by-step knitting program and turn your creativity into wearable pieces, 
@@ -70,7 +72,7 @@ export default async function Home() {
         Let's get creative!
       </p>
       <Link href="/create" >
-        <button className='btn btn-secondary'>
+        <button className='btn btn-primary'>
           Create
         </button>
       </Link>
