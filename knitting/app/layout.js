561b0f3e180fc1b4387952d36b95bf4db272f749
vande <lore.vandenneste@student.kuleuven.be>
2025-11-04 10:13:18 +0100

    navbar on home

--- knitting/app/layout.js
+++ knitting/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div>
+        <Navbar/>
+        </div>
         {children}
       </body>
     </html>
