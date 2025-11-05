468961ae14140e80ddbaad4d072bf75959b3a4d0
kaat.stessens <kaat.stessens@student.kuleuven.be>
2025-11-05 16:27:00 +0100

    little changes create page

--- knitting/app/create/page.tsx
+++ knitting/app/create/page.tsx
@@ -1,5 +1,7 @@
 export default function Page() {
     return(
-        <div className="text-center">Create page...</div>
+        <div className="">
+            Create page...
+        </div>
     );
 }
