e50e621dc04a8dcb2cbfc5837600493cf5e13b74
zoep30 <zoe.pintelon@student.kuleuven.be>
2025-11-07 14:05:56 +0100

    Finished primilary design homepage

--- knitting/app/page.tsx
+++ knitting/app/page.tsx
@@ -13,8 +13,9 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen bg-[url('/background.svg')] bg-cover ">
+
       
-      <div className="relative flex justify-center items-start px-4 py-8 mt-12">
+      <div className="relative flex justify-center items-start px-4 py-8 mt-12 translate-x-20">
           <Image
             src="/HomePageImages/image1.png"
             alt="First decorative image"
@@ -60,30 +61,57 @@ export default async function Home() {
       </div>
 
       <div className="flex flex-col items-center gap-6 px-4 py-8">
-        <div className="card bg-white max-w-6xl w-full p-6 border border-stone-300 rounded-[10px]" style={{ boxShadow: '0px 1px 2px rgba(68, 64, 60, 0.15)' }}>
-          <p className="mb-4 text-center text-xl">Want to stop fast fashion and be more sustainable by making your own beautiful clothes? <br/> Join our <span className='text-orange-700'>step-by-step knitting program</span> and turn your creativity into wearable pieces, 
-           <span className='text-orange-700'> made by you, for you</span>.
-          </p>
-          <div className='flex justify-center'>
-            <Link href="/learn">
-            <button className="btn bg-orange-700 rounded-[10px] text-center text-orange-50 px-3 text-2xl">
-              Learn
-            </button>
-          </Link>
+        <div className="card bg-white max-w-6xl w-full p-6 border border-stone-300 rounded-lg " style={{ boxShadow: '0px 1px 2px rgba(68, 64, 60, 0.15)' }}>
+          <div>
+            <p className="mb-4 text-center text-2xl">Want to stop fast fashion and be more sustainable by making your own beautiful clothes? <br/> Join our <span className='text-orange-700'>step-by-step knitting program</span> and turn your creativity into wearable pieces, 
+              <br/> <span className='text-orange-700'> made by you, for you</span>.
+            </p>
+            <div className='flex justify-center'>
+              <Link href="/learn" className='relative'>
+                <button className="relative z-10 border border-orange-700 border-2 bg-orange-700 rounded-lg text-center text-orange-50 px-4 py-2 text-3xl hover:bg-white hover:border-orange-700 hover:text-orange-700">
+                  Learn
+                </button>
+                <Image
+                    src={"/HomePageImages/naalden.svg"}
+                    alt="naalden"
+                    width={63}
+                    height={75}
+                    className="absolute right-0 -top-7 translate-x-1/2 z-0"
+                  />
+              </Link>
+            </div>
+          </div>
+          
+          <div  className="mt-8">
+            <p className="mb-4 text-center ">Already ready to take on a project? We offer a tool that helps you track your patterns and gather your ideas. 
+              <br/> <span className='text-orange-700'>Let's get creative!</span>
+            </p>
+            <div className='flex justify-center'>
+              <Link href="/create" className='relative'>
+                <Image
+                  src={"/HomePageImages/naalden.svg"}
+                  alt="naalden"
+                  width={43}
+                  height={55}
+                  className="absolute right-0 -top-5 translate-x-1/2 z-0"
+                />
+                <button className="relative z-10 border border-orange-700 border-2 bg-orange-700 rounded-lg text-center text-orange-50 px-2 py-1 text-lg hover:bg-white hover:border-orange-700 hover:text-orange-700">
+                  Create
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
 
-        <div className="card bg-white max-w-5xl w-full p-6 border border-stone-300 rounded-[10px]" style={{ boxShadow: '0px 1px 2px rgba(68, 64, 60, 0.15)' }}>
-          <p className="mb-4 text-center">Already ready to take on a project? We offer a tool that helps you track your patterns ad gather your ideas. 
-            <br/> <span className='text-orange-700'>Let's get creative!</span>
+        <div className="card bg-white max-w-6xl w-full p-6 border border-stone-300 rounded-lg" style={{ boxShadow: '0px 1px 2px rgba(68, 64, 60, 0.15)' }}>
+          <p className="mb-4 text-center">
+            Need a hand with project calculations? Our easy-to-use <Link href={"/calculator"} className='text-orange-700 hover:underline'>calculators</Link> are here to help. 
+            <br/> Making your work lighter and your results spot-on!
           </p>
-          <div className='flex justify-center'>
-            <Link href="/create">
-              <button className="btn bg-orange-700 rounded-[10px] text-center text-orange-50 px-3 text-xl">
-                Create
-              </button>
-            </Link>
-          </div>
+
+          <p className="mb-4 text-center">
+            Curious about a knitting term? Our handy <Link href={"/dictionary"} className='text-orange-700 hover:underline'>dictionary</Link> is here to guide you. <br/> Helping you navigate the world of knitting with ease!
+          </p>
         </div>
       </div>
 
