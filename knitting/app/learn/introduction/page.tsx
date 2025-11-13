c8dcb4d832f68ddd3db39c32f142bb5cfad9e153
Nienke Brandwijk <nienke.brandwijk@student.kuleuven.be>
2025-11-13 20:32:49 +0100

    added course content

--- knitting/app/learn/introduction/page.tsx
+++ knitting/app/learn/introduction/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { getFlatPages } from '../../../src/data/data';
+import { usePathname } from 'next/navigation';
+
 export default function Page() {
+    const pathname = usePathname();
+    const flatPages = getFlatPages();
+    const currentPage = flatPages.find(page => page.path === pathname);
+
     return(
         <div className="flex flex-col justify-center gap-6">
             <h1 className="text-4xl font-bold text-txtBold">
-                1. Introduction
+                {currentPage ? currentPage.title : 'Untitled'}
             </h1>
             
             <div className="text-lg text-txtDefault flex flex-col gap-4">
