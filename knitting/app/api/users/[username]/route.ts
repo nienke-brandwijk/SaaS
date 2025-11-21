2fb9ff265d820b20d95970dc24e86dbe5824001f
vande <lore.vandenneste@student.kuleuven.be>
2025-11-21 14:34:26 +0100

    users part removed

--- knitting/app/api/users/[username]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server';
-import { getUserByUsernameController } from '../../../../src/controller/user.controller';
-
-export async function GET(
-    req: NextRequest, 
-    { params }: any
-) {
-  return getUserByUsernameController(req, { params });
-}
