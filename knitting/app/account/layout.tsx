f2f576908c95a4d1c1a5df84ce951641bff96c69
vande <lore.vandenneste@student.kuleuven.be>
2025-11-21 14:21:14 +0100

    account

--- knitting/app/account/layout.tsx
+++ knitting/app/account/layout.tsx
@@ -1,10 +1,5 @@
-import { getCurrentUser } from '../../lib/auth';
-import { getUserWIPS } from "../../src/controller/wips.controller";
-import Page from './page';
+"use client";
 
-export default async function Layout() {
-  const user = await getCurrentUser();
-  const allWips = await getUserWIPS(user?.id);
-  const finishedWips = allWips.filter((w: any) => w.wipFinished);
-  return <Page user={user} wips={finishedWips} />;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
