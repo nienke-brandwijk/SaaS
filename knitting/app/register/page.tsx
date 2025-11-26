45cbe083e4c4ac69572a7e6375ca251fe0a61a95
kaat.stessens <kaat.stessens@student.kuleuven.be>
2025-11-26 16:55:50 +0100

    fixing register

--- knitting/app/register/page.tsx
+++ knitting/app/register/page.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import RegisterForm from './registerForm';
 
-export default function Page({ searchParams }: { searchParams?: { redirect?: string } }) {
+export default async function Page({ searchParams }: { searchParams?: { redirect?: string } }) {
   const redirectTo = (searchParams && searchParams.redirect) || '/';
   
   return (
