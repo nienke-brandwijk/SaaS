3e94bb836e2f284beb5f14be08c5251a2708e903
ErnestLauwers <lauwers.ernest@hotmail.com>
2025-11-28 15:10:11 +0100

    More Auth

---
+++ knitting/app/api/helper.tsx
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+import { NextRequest, NextResponse } from 'next/server';
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export function authenticate(req: NextRequest) {
+  const token = req.cookies.get('token')?.value;
+  if (!token) {
+    throw new Error('Unauthorized');
+  }
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    return payload;
+  } catch (err) {
+    throw new Error('Invalid token');
+  }
+}
