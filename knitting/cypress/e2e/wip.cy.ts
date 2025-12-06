b38d81cb2972c41d710e00668e4b5ba7210317db
kaat.stessens <kaat.stessens@student.kuleuven.be>
2025-12-06 16:24:59 +0100

    wip.cy.ts some tests in comment

--- knitting/cypress/e2e/wip.cy.ts
+++ knitting/cypress/e2e/wip.cy.ts
@@ -39,7 +39,7 @@ describe.only('WIP (Work In Progress) Page - Essential Tests', () => {
     });
   });
 
-  describe('Image Upload', () => {
+  {/* describe('Image Upload', () => {
     const createTestImage = () => {
       return cy.window().then((win) => {
         return new Promise<Blob>((resolve) => {
@@ -98,34 +98,34 @@ describe.only('WIP (Work In Progress) Page - Essential Tests', () => {
       cy.contains('button', 'Upload image').should('be.visible');
 
     });
-  });
+  }); */}
 
-  describe('Needles Management', () => {
+  {/* describe('Needles Management', () => {
     it('opens modal when clicking add needle button', () => {
       cy.contains('h3', 'Needles').parent().find('button[aria-label="Add needle"]').click();
       cy.contains('h2', 'Add needle').should('be.visible');
     });
-  });
+  }); */}
 
-  describe('Yarn Management', () => {
+  {/*describe('Yarn Management', () => {
     it('opens modal when clicking add yarn button', () => {
       cy.contains('h3', 'Yarn').parent().find('button[aria-label="Add yarn"]').click();
       cy.contains('h2', 'Add yarn').should('be.visible');
     });
-  });
+  }); */}
 
-  describe('Gauge Swatch Management', () => {
+  {/* describe('Gauge Swatch Management', () => {
     it('opens modal when clicking add gauge swatch button', () => {
       cy.contains('h3', 'Gauge swatch').parent().find('button[aria-label="Add gauge swatch"]').click();
       cy.contains('h2', 'Add gauge swatch').should('be.visible');
     });
-  });
+  }); */}
 
-  describe('Size Management', () => {
+  {/* describe('Size Management', () => {
     it('opens modal when clicking add size button', () => {
       cy.contains('h3', 'Size').parent().find('button[aria-label="Add size"]').click();
       cy.contains('h2', 'Add size').should('be.visible');
-    });
+    }); */} 
 
   //  it('adds a size', () => {
   //     cy.contains('h3', 'Size').parent().find('button[aria-label="Add size"]').click();
@@ -144,7 +144,7 @@ describe.only('WIP (Work In Progress) Page - Essential Tests', () => {
   //   });
   });
 
-  describe('Measurements', () => {
+  {/* describe('Measurements', () => {
     it('allows user to enter chest circumference', () => {
       cy.get('.flex-1.space-y-2').eq(0).within(() => {
         cy.get('input').type('90');
@@ -164,19 +164,19 @@ describe.only('WIP (Work In Progress) Page - Essential Tests', () => {
         cy.get('input').should('have.value', '10');
       });
     });
-  });
+  }); */}
 
-  describe('Extra Materials Management', () => {
+  {/* describe('Extra Materials Management', () => {
     it('opens modal when clicking add extra material button', () => {
       cy.contains('h3', 'Extra materials').parent().find('button[aria-label="Add extra material"]').click();
       cy.contains('h2', 'Add extra material').should('be.visible');
     });
-  });
+  }); */}
 
-  describe('Current Position', () => {
+  {/* describe('Current Position', () => {
     it('allows user to add notes about current position', () => {
       cy.contains('h3', 'Current position').parent().find('textarea').type('Test');
       cy.contains('h3', 'Current position').parent().find('textarea').should('have.value', 'Test');
     });
-  });
-});
+  }); */}
+//});
