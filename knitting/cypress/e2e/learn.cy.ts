f6abf4801cc2e53efeedeb1041034d1c9033851d
kaat.stessens <kaat.stessens@student.kuleuven.be>
2025-12-10 17:09:21 +0100

    really fixed test

--- knitting/cypress/e2e/learn.cy.ts
+++ knitting/cypress/e2e/learn.cy.ts
@@ -8,8 +8,9 @@ describe.only('Learn pages - basic correctness', () => {
 
     // Content assertions (use case-insensitive regex to match text snippets)
     cy.contains(/Knitting is a calming/i).should('exist');
-    cy.contains(/Begin by learning the basics/i).should('exist');
-    cy.contains(/Understanding common knitting terms/i).should('exist');
+    cy.contains(/If you're new to knitting/i).should('exist');
+    cy.contains(/Knitting has its own vocabulary/i).should('exist');
+    cy.contains(/Whether you're picking up knitting needles for the very first time/i).should('exist');
   });
 
   it('has learn navigation links and clicking one navigates to another learn page', () => {
